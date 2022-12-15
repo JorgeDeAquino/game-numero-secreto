3a8dd4f636954a6ab5d486ad9403da04fea8500b
Jorge Alexandre <jorgeaamsantos@gmail.com>
2022-12-15 12:31:24 -0300

    add tela de resultado correto e ajustado css

--- app/validacao.js
+++ app/validacao.js
@@ -3,12 +3,23 @@ function verificaSeOChutePossuiUmValorValido(chute) {
     const numero = +chute
 
     if (chuteNan(numero)) {
-        console.log('O valor precisa ser um numero!')
+        elementoChute.innerHTML += `<div>O valor precisa ser um numero!</div>`
     }
 
     if (chuteInvalido(numero)) {
-        console.log(`Valor invalido! O numero secreto deve estar entre ${menorValor} e ${maiorValor}`)
+        elementoChute.innerHTML += `<div>Valor invalido! O numero secreto deve estar entre ${menorValor} e ${maiorValor}</div>`
     }
+
+    if (chuteCerto(numero)) {
+        document.body.innerHTML = `
+        <h2>Parabéns Você Acertou!</h2>
+        <h3>O número secreto é: ${numeroSecreto}</h3>
+        `
+    }
+}
+
+function chuteCerto(numero) {
+    return numero === numeroSecreto
 }
 
 function chuteNan(numero) {

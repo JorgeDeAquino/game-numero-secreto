e50ea726af38ddd7d82ddda8e5bc0fd7f19999ef
Jorge Alexandre <jorgeaamsantos@gmail.com>
2022-12-16 12:50:59 -0300

    add validacao na tela de acerto, add btn reload, add funcao continuar jogando

--- app/validacao.js
+++ app/validacao.js
@@ -14,6 +14,15 @@ function verificaSeOChutePossuiUmValorValido(chute) {
         document.body.innerHTML = `
         <h2>Parabéns Você Acertou!</h2>
         <h3>O número secreto é: ${numeroSecreto}</h3>
+        <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div> O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+        <div> O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
         `
     }
 }
@@ -29,3 +38,9 @@ function chuteNan(numero) {
 function chuteInvalido(numero) {
     return numero > maiorValor || numero < menorValor
 }
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
